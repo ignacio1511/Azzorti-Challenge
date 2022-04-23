952da3954baf70a3cde7acb16117eb5aa9b24e5d
Ignacio de la Cuba <ignaciodelacuba@MacBook-Pro-de-Ignacio-2.local>
2022-04-23 15:50:59 -0500

    pregunta 1

---
+++ pregunta2.py
@@ -0,0 +1,9 @@
+""" 
+Calcular una funcion que bote el promedio de ventas de una campaña  con respecto a la campaña pasada
+
+1. Encontrar el año y numero de la camopaña -> CAMP
+2. Buscar todas las campañas anteriores a esa
+3. Sumar el precio de cada una 
+4. Dividirlo entre el total de datos analizados
+
+"""
